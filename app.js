91413052081288df0b0dc50a58a259a151e04ff8
Jordan <JordanLesaca@Gmail.Com>
2017-11-22 18:45:46 -0800

    remade repo so that mimages are all in one folder. couldnt figure out a faster way to move the images from one folder to the other and I accidentally did some command the messed everything up....

---
+++ app.js
@@ -0,0 +1,159 @@
+'use strict';
+var allProducts = [];
+var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'sweepers', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+function Product(name, path) {
+  this.name = name;
+  this.path = 'images/' + this.name + '.jpg';
+  this.clicked = 0;
+  this.votes = 0;
+  allProducts.push(this);
+}
+
+(function() {
+  for(var i in productNames){
+    new Product(productNames[i]);
+  }
+})();
+
+var tracker ={
+  imagesEl: document.getElementById('images'),
+  resultsEl: document.getElementById('results'),
+  clickCount: 0,
+
+  imageOne: document.createElement('img'),
+  imageTwo: document.createElement('img'),
+  imageThree: document.createElement('img'),
+
+  getRandomIndex: function() {
+    return Math.floor(Math.random() * allProducts.length);
+  },
+
+  displayImages: function() {
+    var idOne = this.getRandomIndex();
+    var idTwo = this.getRandomIndex();
+    var idThree = this.getRandomIndex();
+
+    if(idOne === idTwo || idOne === idThree || idTwo === idThree) {
+      this.displayImages();
+      return;
+    }
+
+    this.imageOne.src = allProducts[idOne].path;
+    this.imageTwo.src = allProducts[idTwo].path;
+    this.imageThree.src = allProducts[idThree].path;
+
+    this.imageOne.id = allProducts[idOne].name;
+    this.imageTwo.id = allProducts[idTwo].name;
+    this.imageThree.id = allProducts[idThree].name;
+
+    this.imagesEl.appendChild(this.imageOne);
+    this.imagesEl.appendChild(this.imageTwo);
+    this.imagesEl.appendChild(this.imageThree);
+  },
+
+onClick: function(event) {
+    console.log(event.target.id);
+
+    if(event.target.id === 'images') {
+      return;
+    } else {
+      tracker.clickCount++;
+
+      for(var i in allProducts) {
+        if(event.target.id === allProducts[i].name) {
+          allProducts[i].votes++;
+        }
+      }
+      tracker.displayImages();
+    }
+    if (tracker.clickCount === 5){
+      var ctx = document.getElementById('myChart');
+      var options = {
+        type: 'bar',
+        data: {
+          labels: productNames,
+          datasets: [{
+            label: 'Votes Tallied',
+            data: allProducts.map(function(x) {return x.votes;}),
+            backgroundColor: [
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+            ],
+            borderColor: [
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+              'rgb(128, 0, 32)',
+              'rgb(0, 0, 0)',
+              'rgb(255, 255, 255)',
+            ],
+            borderWidth: 1
+          },
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      };
+      var myChart = new Chart(ctx, options);
+      tracker.imagesEl.removeEventListener('click', tracker.onClick);
+    }
+  }
+};
+
+localStorageSet: function() {
+    localStorage.clear ();
+    var encodeAllProducts = JSON.stringify(allProducts);
+    localStorage.setItem('AllProducts', encodeAllProducts);
+  },
+
+ localStorageGet: function () {
+    var obj = JSON.parse(localStorage.getItem('AllProducts'));
+    if (obj !== null){
+      allProducts = obj;
+    }
+  }
+
+tracker.imagesEl.addEventListener('click', tracker.onClick);
+tracker.displayImages();
